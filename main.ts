622dc352ac2a67219fbadbd86d7e12f6b4acef1c
kennedyfitzz <82603835+kennedyfitzz@users.noreply.github.com>
2021-05-10 10:54:17 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    let mySprite: Sprite = null
+    mySprite.setPosition(0, 0)
+})
+let randomNumber = randint(1, 100)
+let anotherRandom = randint(20, 30)
+game.splash("Random Number is" + " " + anotherRandom)
+game.splash("Random Number is" + " " + randomNumber)
